f84a4cc443dcf5d4d9d760ff403e806dc2d2ae29
Victoria <vmoix001@mail.ru>
2021-09-30 01:03:36 +0700

    r

--- src/com/company/PhoneBook.java
+++ src/com/company/PhoneBook.java
@@ -1,7 +1,129 @@
 package com.company;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class PhoneBook {
-    public static void main(String[] args) {
+    private ArrayList<Record> list = new ArrayList<>();
 
+    public static void main(String[] args) {
+        Record rec1 = new Record(123456789, "89137649297", "Victoria");
+        Record rec2 = new Record(123455678, "89137649287", "Alex");
+        Record rec3 = new Record(123456670, "89139999999", "Blair");
+        PhoneBook phoneBook1 = new PhoneBook(new ArrayList<>());
+        try {
+            phoneBook1.createRecord(rec1);
+            phoneBook1.createRecord(rec2);
+            phoneBook1.createRecord(rec3);
+            System.out.println(phoneBook1.getAllRecords());
+        }
+        catch(PhoneNumberAlreadyExists ex){
+            System.out.println(ex.getMessage());
+        }
+        try {
+            phoneBook1.updateRecord(rec1);
+        } catch (RecordNotValid e) {
+            e.printStackTrace();
+        }
+        System.out.println(phoneBook1.getAllRecords());
+        phoneBook1.deleteRecord(123456670);
+        System.out.println(phoneBook1.getAllRecords());
+    }
+
+    public PhoneBook(ArrayList<Record> ls){
+        list = ls;
+    }
+
+    public String getAllRecords(){
+        String result = "";
+        for(Record i: list){
+           result += "Идентификатор: " + Long.toString(i.getId())+ ", Имя: " + i.getId() + ", Номер телефона: " +
+                   i.getPhoneNumber() + "\n";
+        }
+        return result;
+    }
+
+    public void createRecord (Record record) throws PhoneNumberAlreadyExists{
+        for(Record i: list){
+            if(i.getPhoneNumber() == record.getPhoneNumber()) throw new PhoneNumberAlreadyExists("Телефонный номер" +
+                    " уже существует");
+        }
+        list.add(record);
+    }
+
+    public void updateRecord(Record record) throws RecordNotFound, RecordNotValid{
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите id: ");
+        String num = in.nextLine();
+        System.out.print("Введите имя: ");
+        String namee = in.nextLine();
+        if(num == "" || namee == "") throw new RecordNotValid("Не заполнены имя и/или номер телефона");
+        int index = 0;
+        for(Record i: list){
+            index++;
+            if(i.getId() == record.getId()){
+                i.setPhoneNumber(num);
+                i.setName(namee);
+            }
+        }
+        if (index == list.size()) throw new RecordNotFound("Не существующий номер");
+    }
+
+    public void deleteRecord(long id){
+        int index = 0;
+        for(Record i: list){
+            index ++;
+            if(Long.toString(i.getId()).equals(Long.toString(id))){
+                list.remove(i);
+            }
+        }
+        if(index== list.size()) throw new RecordNotFound("Не существующий номер");
+    }
+
+
+
+    public static class Record {
+        private long id;
+        private String phoneNumber;
+        private String name;
+
+        public Record(long i, String pn, String n) {
+            id = i;
+            phoneNumber = pn;
+            name = n;
+        }
+
+        @Override
+        public String toString() { return id + " " + phoneNumber + " " + name;}
+        public long getId(){ return this.id;}
+        public String getPhoneNumber(){ return this.phoneNumber;}
+        public String getName(){ return this.name;}
+        public void setId(long id){
+            this.id = id;
+        }
+        public void setPhoneNumber(String phoneNumber){
+            this.phoneNumber = phoneNumber;
+        }
+        public void setName(String name){
+            this.name = name;
+        }
+
+    }
+}
+
+class PhoneNumberAlreadyExists extends Exception{
+    public PhoneNumberAlreadyExists(String message){
+        super(message);
+    }
+}
+
+class RecordNotValid extends Exception{
+    public RecordNotValid(String message){
+        super(message);
+    }
+}
+
+class RecordNotFound extends RuntimeException{
+    public RecordNotFound(String message){
+        super(message);
     }
 }
